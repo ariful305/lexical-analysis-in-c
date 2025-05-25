82fd6b58199d00f71baa1f2542a5d7d84c3ce64e
Md Ariful Islam <mdarifulislam3579@gmail.com>
2025-05-25 22:04:37 +0600

    update

--- variable-length-fixed/output.c
+++ variable-length-fixed/output.c
@@ -2,10 +2,10 @@
 
 #define MAX_SIZE 100 
 
-int main() {
-    int veryL = 5;
-    float anoth = 3.14;
-    long long anoth = 3.14;
+int mai() {
+    int ver = 5;
+    float ano = 3.14;
+    long long ano = 3.14;
     return 0;
 }
 
